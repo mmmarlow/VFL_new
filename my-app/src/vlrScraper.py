9f214f1f8d32b6199b3936b09d862519f0c331e1
marlow <101742018+mmmarlow@users.noreply.github.com>
2024-04-13 21:34:34 +0100

    Cleaning up

--- my-app/src/vlrScraper.py
+++ my-app/src/vlrScraper.py
@@ -2,13 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-#open with google sheets
-#line 47 for url control
+#open with google sheets to test
 
 def scrapeStats(url):
-
     getResponse = requests.get(url)
-
     #verifying successful request response
     if getResponse.status_code == 200:
         #parse HTML content
@@ -31,18 +28,29 @@ def scrapeStats(url):
         print("Request Error")
         return None, None
 
-def save_to_csv(headers, rows, fileName):
+def clean_data(headers, rows):
+    #appending new team column
+    for row in rows:
+        player_name = row[0]
+        team_name = player_name.split()[-1]
+        row.append(team_name)
+    #deleting redundant agents column
+    agents_index = headers.index('Agents')
+    for row in rows:
+        del row[agents_index]
+    del headers[agents_index]
 
+def save_to_csv(headers, rows, fileName):
     with open(fileName, 'w', newline='') as csvFile:
         writer = csv.writer(csvFile)
-         #write headers and rows
-        writer.writerow(headers)
+        #write headers and rows
+        #create team header
+        writer.writerow(headers + ['Team'])
         for row in rows:
             writer.writerow(row)
     print(f"Data has been saved to {fileName}")
 
 def main():
-
     #change this depending on the vlr stats page that you want it to scrape from
     url = "https://www.vlr.gg/event/stats/1921/champions-tour-2024-masters-madrid?exclude=&min_rounds=0&agent=all"
     #created CSV file name 
@@ -51,9 +59,10 @@ def main():
     headers, rows = scrapeStats(url)
     #check data is retrieved successfully
     if headers and rows:
+        #clean data
+        clean_data(headers, rows)
         #save data to CSV file
         save_to_csv(headers, rows, fileName)
-
 
 if __name__ == "__main__":
     main()
