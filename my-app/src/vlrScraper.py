115d88815e8d01720e35415550ad247c559485f9
marlow <101742018+mmmarlow@users.noreply.github.com>
2024-04-13 15:50:14 +0100

    Update vlrScraper.py

--- my-app/src/vlrScraper.py
+++ my-app/src/vlrScraper.py
@@ -2,14 +2,41 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+def scrapeStats(url):
+
+    getResponse = requests.get(url)
+
+    #checking for a successful response from the request
+    if getResponse.status_code == 200:
+        doc = BeautifulSoup(getResponse.text, "html.parser")
+        print(doc.prettify())
 
 
-#change this depending on the vlr stats page that you want it to scrape from
-url = "https://www.vlr.gg/event/stats/1921/champions-tour-2024-masters-madrid?exclude=&min_rounds=0&agent=all"
+def save_to_csv(headers, rows, fileName):
 
-results = requests.get(url)
-doc = BeautifulSoup(results.text, "html.parser")
-print(doc.prettify())
+    with open(fileName, 'w', newLine='') as csvFile:
+        writer = csv.writer(csvFile)
+
+def main():
+
+    #change this depending on the vlr stats page that you want it to scrape from
+    url = "https://www.vlr.gg/event/stats/1921/champions-tour-2024-masters-madrid?exclude=&min_rounds=0&agent=all"
+
+    fileName = 'vlr_stats.csv'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #need to save the data <tbody> tags on the page, which contain <tr>
 #tags, which hold the player stats
