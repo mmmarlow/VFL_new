caf126e0bf787640b84b573fa0d4b7c0e12a57fc
marlow <101742018+mmmarlow@users.noreply.github.com>
2024-04-14 23:27:19 +0100

    Website created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,96 @@
-  function App() {
+import "./App.css";
+import logo from './logo.png';
+
+function App() {
+
   return (
-    null
+
+    <div>
+      <header className="header">
+        <nav className="inner-header">
+          <img src={logo} alt="Logo" className="main-page-header-logo"></img>
+          <ul className="nav-list">
+            <li id="create-team-nav-button">
+              <a href="#" onclick="createTeam(); return false;">Create Team</a>
+            </li>
+            <li id="view-team-nav-button">
+              <a href="#" onclick="viewTeam(); return false;">View Team</a>
+            </li>
+            <li id="stats-nav-button">
+              <a href="#" onclick="stats(); return false;">Stats</a>
+            </li>
+            <li id="leaderboard-nav-button">
+              <a href="#" onclick="leaderboard(); return false;">Global Leaderboard</a>
+            </li>
+            <li id="forums-nav-button">
+              <a href="#" onclick="forums(); return false;">Forums</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div id="wrapper">
+        <div id="main-page-text">
+          <h1 id="main-page-title">A New Premier Valorant Fantasy League</h1>
+          <p id="main-page-para">Create Team! Compete! Score!</p>
+        </div>
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
+
   );
+
+}
+
+function createTeam() {
+
+  return (
+
+    null
+
+  );
+
+}
+
+function viewTeam() {
+
+  return (
+
+    null
+
+  );
+
+}
+
+function stats() {
+
+  return (
+
+    null
+
+  );
+
+}
+
+function leaderboard() {
+
+  return (
+
+    null
+
+  );
+
+}
+
+function forums() {
+
+  return (
+
+    null
+
+  );
+
 }
 
 export default App;
